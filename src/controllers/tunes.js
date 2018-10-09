97846eb8f072a9d54e7f73552b84c10489e2ef2c
Christophe Porteneuve <tdd@tddsworld.com>
2018-10-09 22:14:27 +0200

    Étape 4 : Hypermedia

--- src/controllers/tunes.js
+++ src/controllers/tunes.js
@@ -1,9 +1,14 @@
+import { getPageDescriptors } from '../util/pagination.js'
 import TUNES from '../../fixtures/tunes.js'
 
+let router
+
 export function setupTuneRoutes(server) {
-  server.get('/tunes', listTunes)
-  server.post('/tunes', createTune)
-  server.post('/tunes/:tuneId/votes', voteOnTune)
+  router = server.router
+
+  server.get({ name: 'listTunes', path: '/tunes' }, listTunes)
+  server.post({ name: 'createTune', path: '/tunes' }, createTune)
+  server.post({ name: 'voteOnTune', path: '/tunes/:tuneId/votes' }, voteOnTune)
 }
 
 async function createTune(req, res) {
@@ -12,7 +17,24 @@ async function createTune(req, res) {
 }
 
 async function listTunes(req, res) {
-  res.send({ tunes: [{ artist: 'Alan Walker', title: 'Sky' }] })
+  const { page = 1, pageSize = 10 } = req.query
+
+  const links = getPageDescriptors({
+    page,
+    pageSize,
+    totalCount: TUNES.length,
+  })
+  const tunes = [...TUNES]
+    .sort((t1, t2) => t2.createdAt.localeCompare(t1.createdAt))
+    .slice((page - 1) * pageSize, page * pageSize)
+
+  for (const [rel, query] of Object.entries(links)) {
+    const url = router.render('listTunes', {}, query)
+    links[rel] = url
+    res.link(url, rel)
+  }
+
+  res.send({ links, tunes })
 }
 
 async function voteOnTune(req, res) {
