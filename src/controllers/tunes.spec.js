692b1f516bea58cb7e2bb4b02fb2fb2cc4b4e26b
Christophe Porteneuve <tdd@tddsworld.com>
2018-10-09 22:16:33 +0200

    Début exercice étape 5 : tests Hypermedia

---
+++ src/controllers/tunes.spec.js
@@ -0,0 +1,42 @@
+import request from 'supertest'
+
+import { createServer } from '../app.js'
+
+const app = createServer()
+
+describe('Tunes controller', () => {
+  describe('Tune listings', () => {
+    it('should order recent-first by default', () => {
+      return request(app)
+        .get(app.router.render('listTunes'))
+        .expect(200)
+        .then((res) => {
+          expect(res.body.tunes).toMatchObject([
+            { title: 'World Falls Apart' },
+            { title: 'Kenia' },
+            { title: 'Sky' },
+          ])
+        })
+    })
+
+    it('should provide earlier links when beyond page 1', () => {
+      const expectedLinks = {
+        first: app.router.render('listTunes', {}, { page: 1, pageSize: 1 }),
+        prev: app.router.render('listTunes', {}, { page: 2, pageSize: 1 }),
+      }
+      const expectedLinkHeader = Object.entries(expectedLinks)
+        .map(([rel, url]) => `<${url}>; rel="${rel}"`)
+        .join(', ')
+
+      return request(app)
+        .get(app.router.render('listTunes', {}, { page: 3, pageSize: 1 }))
+        .expect(200)
+        .expect('Link', expectedLinkHeader)
+        .then((res) => {
+          expect(res.body.links).toEqual(expectedLinks)
+        })
+    })
+
+    it.todo('should provide later links when before last page')
+  })
+})
