012f942a9fc39ca8f9bc5e15050d565f64fbd7ab
Christophe Porteneuve <tdd@tddsworld.com>
2018-10-10 09:52:00 +0200

    Début étape 24 : authentification et autorisation GraphQL

---
+++ src/util/graphql-jwt.js
@@ -0,0 +1,75 @@
+import apollo from 'apollo-server'
+import { config as configEnv } from 'dotenv-safe'
+import { defaultFieldResolver } from 'graphql'
+import errors from 'restify-errors'
+import jwt from 'jsonwebtoken'
+
+const { gql, SchemaDirectiveVisitor } = apollo
+
+configEnv()
+
+export async function getUserFromReq({ req }) {
+  // FIXME: Return a context object, with a valid `user` key if we have an authenticated user
+}
+
+const authTypeDefs = gql`
+  """
+  Ensures there is an authenticated used, possibly with a given role,
+  before accessing a field.
+  """
+  directive @auth(role: Role = ADMIN) on OBJECT | FIELD_DEFINITION
+`
+
+class AuthDirective extends SchemaDirectiveVisitor {
+  ensureFieldsWrapped(objectType) {
+    if (objectType._authFieldsWrapped) return
+    objectType._authFieldsWrapped = true
+
+    const fields = objectType.getFields()
+
+    Object.keys(fields).forEach((fieldName) => {
+      const field = fields[fieldName]
+      const { resolve = defaultFieldResolver } = field
+      field.resolve = async function (...args) {
+        const requiredRole =
+          field._requiredAuthRole || objectType._requiredAuthRole
+
+        if (!requiredRole) {
+          return resolve.apply(this, args)
+        }
+
+        const context = args[2]
+
+        if (!context.user) {
+          throw new errors.NotAuthorizedError(
+            `${fieldName} requires authentication`
+          )
+        }
+
+        const role = requiredRole.toLowerCase()
+        if (role && !(context.user.roles || []).includes(role)) {
+          throw new errors.NotAuthorizedError(
+            `${fieldName} requires the authenticated user to have role ${requiredRole}`
+          )
+        }
+
+        return resolve.apply(this, args)
+      }
+    })
+  }
+
+  visitFieldDefinition(field, details) {
+    this.ensureFieldsWrapped(details.objectType)
+    field._requiredAuthRole = this.args.role
+  }
+
+  visitObject(type) {
+    this.ensureFieldsWrapped(type)
+    type._requiredAuthRole = this.args.role
+  }
+}
+
+export const auth = {
+  directive: AuthDirective,
+  typeDefs: authTypeDefs,
+}
